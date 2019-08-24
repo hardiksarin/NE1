fbb226b18196412f3bb63b70c6780306cdfdbc1f
hardiksarin <you@example.comsarinhardy@gmail.comgit config --global user.email you@example.comsarinhardy@gmail.com>
2019-08-24 21:56:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ne/parser/JSONParser.java
@@ -0,0 +1,65 @@
+package com.example.ne.parser;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+public class JSONParser {
+
+    private static final String MAIN_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=o23eIVVrg2ip9VdoZjP2vl25Fj-uX_IEbwCG8oUBGgEkgwT9zgeEenJjDRAAnc1mZYUx-L-LV7IPyYntYlY02PsUc13CbjsPOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1GhPSVukpSQTydEwAEXFXgt_wltjJcH3XHUaaPC1fv5o9XyvOto09QuWI89K6KjOu0SP2F-BdwU_SDq_Bud00ul3QljTNp45aUPF1dbitjPFEGz5VPTdgPkW7agUNYZ7OSZCYO9PChg5y7FLqOV0Tk27B8Rh4QJTQ&lib=MnrE7b2I2PjfH799VodkCPiQjIVyBAxva";
+
+    public static final String TAG = "TAG";
+
+    private static final String KEY_USER_ID = "user_id";
+
+    private static Response response;
+
+    public static JSONObject getDataFromWeb() {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(MAIN_URL)
+                    .build();
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        } catch (@NonNull IOException | JSONException e) {
+            Log.e(TAG, "" + e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    public static JSONObject getDataById(int userId) {
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody formBody = new FormEncodingBuilder()
+                    .add(KEY_USER_ID, Integer.toString(userId))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(MAIN_URL)
+                    .post(formBody)
+                    .build();
+
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, "" + e.getLocalizedMessage());
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/ne/util/Keys.java
@@ -0,0 +1,10 @@
+package com.example.ne.util;
+
+
+public class Keys {
+
+    public static final String KEY_CONTACTS = "Sheet1";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_COUNTRY = "country";
+
+}
